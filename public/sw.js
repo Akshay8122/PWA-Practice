323bf5d71297ced9356ecdf76e835dd873913af4
Akshay8122 <akshay.p@simformsolutions.com>
2024-02-22 14:08:27 +0530

    feat: service worker caching added

---
+++ public/sw.js
@@ -0,0 +1,63 @@
+var CACHE_STATIC_NAME = "static-v2";
+var CACHE_DYNAMIC_NAME = "dynamic-v4";
+
+self.addEventListener("install", function (event) {
+  console.log("[Service Worker] Installing Service Worker ...", event);
+  event.waitUntil(
+    caches.open(CACHE_STATIC_NAME).then(function (cache) {
+      console.log("[Service worker] PreCaching App shell");
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/src/js/app.js",
+        "/src/js/feed.js",
+        "/src/js/fetch.js",
+        "/src/js/promise.js",
+        "/src/css/app.css",
+        "/src/css/feed.css",
+        "/src/js/material.min.js",
+        "/src/images/main-image-ig.jpg",
+        "/src/sf.boat.jpg",
+        "https://fonts.googleapis.com/css?family=Roboto:400,700",
+        "https://fonts.googleapis.com/icon?family=Material+Icons",
+        "https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css",
+      ]);
+    })
+  );
+});
+
+self.addEventListener("activate", function (event) {
+  console.log("[Service worker] is activating ...", event);
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(
+        keyList.map(function (key) {
+          if (key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME) {
+            console.log("[Service worker] removing old caches", key);
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener("fetch", function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function (response) {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request)
+          .then(function (res) {
+            return caches.open(CACHE_DYNAMIC_NAME).then(function (cache) {
+              cache.put(event.request.url, res.clone());
+              return res;
+            });
+          })
+          .catch(function (err) {});
+      }
+    })
+  );
+});
