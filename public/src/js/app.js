553f30dce23825773095b4ef369c2c42255bd030
akshay.p <akshay.p@simformsolutions.com>
2024-08-05 15:13:53 +0530

    feat: notification options added

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,24 +1,78 @@
-
 var deferredPrompt;
+var enableNotificationsButtons = document.querySelectorAll(
+  ".enable-notifications"
+);
 
 if (!window.Promise) {
   window.Promise = Promise;
 }
 
-if ('serviceWorker' in navigator) {
+if ("serviceWorker" in navigator) {
   navigator.serviceWorker
-    .register('/sw.js')
+    .register("/sw.js")
     .then(function () {
-      console.log('Service worker registered!');
+      console.log("Service worker registered!");
     })
-    .catch(function(err) {
+    .catch(function (err) {
       console.log(err);
     });
 }
 
-window.addEventListener('beforeinstallprompt', function(event) {
-  console.log('beforeinstallprompt fired');
+window.addEventListener("beforeinstallprompt", function (event) {
+  console.log("beforeinstallprompt fired");
   event.preventDefault();
   deferredPrompt = event;
   return false;
 });
+
+function displayConfirmNotifications() {
+  if ("serviceWorker" in navigator) {
+    var options = {
+      body: "You successfully subscribed to our Notification service",
+      icon: "/src/images/icons/app-icon-96x96.png",
+      image: "/src/images/sf-boat.jpg",
+      dir: "ltr",
+      lang: "en-US",
+      vibrate: [100, 50, 200],
+      badge: "/src/images/icons/app-icon-96x96.png",
+      tag: "confirm-notification",
+      renotify: true,
+      action: [
+        {
+          action: "confirm",
+          title: "Okay",
+          icon: "/src/images/icons/app-icon-96x96.png",
+        },
+        {
+          action: "cancel",
+          title: "Cancel",
+          icon: "/src/images/icons/app-icon-96x96.png",
+        },
+      ],
+    };
+    navigator.serviceWorker.ready.then(function (swreg) {
+      swreg.showNotification("Successfully subscribed (from SW)!!", options);
+    });
+  }
+}
+
+function askForNotificationPermission() {
+  Notification.requestPermission(function (result) {
+    console.log("User Choice", result);
+    if (result !== "granted") {
+      console.log("No notification permission granted!");
+    } else {
+      displayConfirmNotifications();
+    }
+  });
+}
+
+if ("Notification" in window) {
+  for (var i = 0; i < enableNotificationsButtons.length; i++) {
+    enableNotificationsButtons[i].style.display = "inline-block";
+    enableNotificationsButtons[i].addEventListener(
+      "click",
+      askForNotificationPermission
+    );
+  }
+}
