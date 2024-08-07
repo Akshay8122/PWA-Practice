907c546b9ff6c1b3e7991ed90fecdd4f352a66af
akshay.p <akshay.p@simformsolutions.com>
2024-08-07 16:37:50 +0530

    feat:  capture btn logic added

--- public/src/js/feed.js
+++ public/src/js/feed.js
@@ -41,6 +41,23 @@ function initializeMedia() {
     });
 }
 
+captureButton.addEventListener("click", function (event) {
+  canvasElement.style.display = "block";
+  videoPlayer.style.display = "none";
+  captureButton.style.display = "none";
+  var context = canvasElement.getContext("2d");
+  context.drawImage(
+    videoPlayer,
+    0,
+    0,
+    canvas.width,
+    videoPlayer.videoHeight / (videoPlayer.videoWidth / canvas.width)
+  );
+  videoPlayer.srcObject.getVideoTracks().forEach(function (track) {
+    track.stop();
+  });
+});
+
 function openCreatePostModal() {
   // createPostArea.style.display = 'block';
   // setTimeout(function() {
