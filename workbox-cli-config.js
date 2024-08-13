f769331ad0a79a0be55a8c6038329705fc7c629c
akshay.p <akshay.p@simformsolutions.com>
2024-08-13 19:00:27 +0530

    feat: configuring workbox precaching

--- workbox-cli-config.js
+++ workbox-cli-config.js
@@ -1,10 +1,6 @@
 module.exports = {
-  "globDirectory": "public/",
-  "globPatterns": [
-    "**/*.{html,ico,json,css,png,jpg,js}"
-  ],
-  "swDest": "public/service-worker.js",
-  "globIgnores": [
-    "../workbox-cli-config.js"
-  ]
+  globDirectory: "public/",
+  globPatterns: ["**/*.{html,ico,json,css,js}", "src/images/*.{jpg,png}"],
+  swDest: "public/service-worker.js",
+  globIgnores: ["../workbox-cli-config.js", "help/**"],
 };
