f3d8650e94b9a7e27931542c36c72c124da9c1d9
akshay.p <akshay.p@simformsolutions.com>
2024-08-15 10:20:48 +0530

    feat : handle background sync and push notification

--- public/sw-base.js
+++ public/sw-base.js
@@ -71,3 +71,104 @@ workboxSW.router.registerRoute(
 );
 
 workboxSW.precache([]);
+
+self.addEventListener("sync", function (event) {
+  console.log("[Service Worker] Background syncing", event);
+  if (event.tag === "sync-new-posts") {
+    console.log("[Service Worker] Syncing new Posts");
+    event.waitUntil(
+      readAllData("sync-posts").then(function (data) {
+        for (var dt of data) {
+          console.log(dt, "dt>>>");
+
+          fetch(
+            "https://pwagram-b89fc-default-rtdb.firebaseio.com/pwagram/posts.json",
+            {
+              method: "POST",
+              headers: {
+                "Content-Type": "application/json",
+                Accept: "application/json",
+              },
+              body: JSON.stringify({
+                id: dt.id,
+                title: dt.title,
+                location: dt.location,
+                image:
+                  "https://firebasestorage.googleapis.com/v0/b/pwagram-b89fc.appspot.com/o/sf-boat.jpg?alt=media&token=b8e0516a-81c8-408b-bd95-bf4ba367e308",
+                rawLocationLat: toString(dt.rawLocation.lat),
+                rawLocationLng: toString(dt.rawLocation.lng),
+              }),
+            }
+          )
+            .then(function (res) {
+              console.log("Sent data", res);
+              if (res.ok) {
+                deleteItemFromData("sync-posts", dt.id); // Isn't working correctly!
+              }
+            })
+            .catch(function (err) {
+              console.log("Error while sending data", err);
+            });
+        }
+      })
+    );
+  }
+});
+
+self.addEventListener("notificationclick", function (event) {
+  var notification = event.notification;
+  var action = event.action;
+
+  console.log(notification);
+
+  if (action === "confirm") {
+    console.log("Confirm was chosen");
+    notification.close();
+  } else {
+    console.log(action);
+    event.waitUntil(
+      clients.matchAll().then(function (clis) {
+        var client = clis.find(function (c) {
+          return c.visibilityState === "visible";
+        });
+
+        if (client !== undefined) {
+          client.navigate("http://localhost:8000");
+          client.focus();
+        } else {
+          clients.openWindow("http://localhost:8000");
+        }
+        notification.close();
+      })
+    );
+  }
+});
+
+self.addEventListener("notificationclose", function (event) {
+  console.log("Notification closed", event);
+});
+
+self.addEventListener("push", function (event) {
+  console.log("Push Notification received", event);
+
+  var data = {
+    title: "New!",
+    content: "Something new happened!",
+    openUrl: "/",
+  };
+
+  if (event.data) {
+    data = JSON.parse(event.data.text());
+  }
+
+  var options = {
+    body: data.content,
+    icon: "/src/images/icons/app-icon-96x96.png",
+    badge: "/src/images/icons/app-icon-96x96.png",
+    data: {
+      url: data.openUrl,
+    },
+  };
+
+  event.waitUntil(self.registration.showNotification(data.title, options));
+});
